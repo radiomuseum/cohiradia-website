c57d1a689cc6a2444149fabd9aed68a6ac1dc136
Ueli Kurmann <ueli.kurmann@gmail.com>
2026-01-25 16:29:55 +0100

    Uploader korrekt anzeigen

---
+++ assets/js/base.js
@@ -0,0 +1,13 @@
+// Cohiradia Radio Archive base JavaScript
+window.Cohiradia = window.Cohiradia || {};
+
+Cohiradia.baseUrl = "https://cohiradia.radiomuseum.org";
+
+Cohiradia.users = {
+    "0": "-",
+    "318": "Claus Peter Gallenmiller",
+    "3642": "Hermann Scharfetter",
+    "19367": "Stan Roberts",
+    "22152": "Walter Barteczek",
+    "33528": "Gianni Müller"
+};
